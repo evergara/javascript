8e5372f42c2048984396482d888b6ee205817608
Emerson vergara <emerson.vergara>
2023-09-22 16:41:19 -0500

    Commit - Promise - Promise - Promise All - Promise Race

--- 06-advanced-app/src/concepts/03-promise.js
+++ 06-advanced-app/src/concepts/03-promise.js
@@ -1,16 +1,88 @@
+import { heroes } from "../data/heroe";
 /**
  *
  * @param {HTMLDivElement} element
  */
-export const promiseCompoponent = (element) => {
-  console.log("Promise commponent");
-};
-
 
 /**
  *
  * @param {string} id
+ * @returns {Promise}
  */
 const findHero = (id) => {
-  const hero = heroes.find((hero) => hero.id === id);
-}
+  return new Promise((resolve, reject) => {
+    const hero = heroes.find((hero) => hero.id === id);
+    return hero ? resolve(hero) : reject(`Hero with id ${id} not found`);
+  });
+};
+
+export const promiseCompoponent = (element) => {
+  console.log("Promise commponent");
+  const heroId = "5d86371f1efebc31def272e2";
+  const heroIdTwo = "5d86371f2343e37870b91ef1";
+
+
+  Promise.all([findHero(heroId), findHero(heroIdTwo)])
+    .then(([hero, hero2]) => {
+      renderHeroTwo(element, hero, hero2);
+    })
+    .catch((error) => {
+      renderHeroError(element, error);
+    });
+};
+
+export const promiseCompoponent2 = (element) => {
+  console.log("Promise commponent");
+  const heroId = "5d86371f1efebc31def272e2";
+  const heroIdTwo = "5d86371f2343e37870b91ef1";
+
+  let heroTemp;
+  findHero(heroId)
+    .then((hero) => {
+      heroTemp = hero;
+      return findHero(heroIdTwo);
+    })
+    .then((hero) => {
+      renderHeroTwo(element, heroTemp, hero);
+    })
+    .catch((error) => {
+      renderHeroError(element, error);
+    });
+};
+
+export const promiseCompoponent1 = (element) => {
+  console.log("Promise commponent");
+  const heroId = "5d86371f1efebc31def272e2";
+
+  findHero(heroId)
+    .then((hero) => {
+      renderHero(element, hero);
+    })
+    .catch((error) => {
+      renderHeroError(element, error);
+    });
+};
+
+const renderHeroTwo = (element, hero, heroTwo) => {
+  element.innerHTML = `${hero.name} / ${heroTwo.name}`;
+};
+const renderHero = (element, hero) => {
+  element.innerHTML = `Heroe: ${hero.name}`;
+};
+const renderHeroError = (element, error) => {
+  element.innerHTML = `<h3> ${error}<h3>`;
+};
+
+// export const promiseCompoponent = (element) => {
+//   console.log("Promise commponent");
+//   const heroId = "5d86371f1efebc31def272e2-";
+
+//   findHero(heroId)
+//     .then((hero) => {
+//       const html = `Heroe: ${hero.name}`;
+//       element.innerHTML = html;
+//     })
+//     .catch((error) => {
+//       element.innerHTML = error;
+//     });
+// };
